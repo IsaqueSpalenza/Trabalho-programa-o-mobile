b9c8b910a1d0649358db13d14c8cd3449382283e
Felipe Saadi <felipesaadi777@gmail.com>
2025-09-19 11:17:28 -0300

    Repositorio

---
+++ app/src/main/java/com/example/testando/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.testando;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button hist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        meuBotao = findViewById(R.id.hist);
+        meuBotao.setOnClickListener(newView.OnClickListener()) {
+            @Override
+            public void OnClick(View v)
+            {
+
+                Toast.makeText(MainActivity.this, "Botão Clicando!", Toast.LENGTH_SHORT).show();
+            }
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+    }
+}
